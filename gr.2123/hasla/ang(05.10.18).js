7dce84472ed9e9a70fc8dae6e5ca04f2c2fc9c64
matikanek <291564.umk@gmail.com>
2020-05-30 14:46:13 +0200

    Adding 5 part.

---
+++ gr.2123/hasla/ang(05.10.18).js
@@ -0,0 +1,153 @@
+ile_hasel = 65;
+
+var haslo = new Array(ile_hasel);
+// Słówka z zajęć angielskiego
+haslo[0] = "output";
+haslo[1] = "far-fetched";
+haslo[2] = "proliferate";
+haslo[3] = "debris";
+haslo[4] = "decay";
+haslo[5] = "dense";
+haslo[6] = "lead";
+haslo[7] = "particles";
+haslo[8] = "high-attitude";
+haslo[9] = "robust";
+haslo[10] = "flimsy";
+haslo[11] = "guidelines";
+haslo[12] = "concrete";
+haslo[13] = "resume";
+haslo[14] = "screening";
+haslo[15] = "indication";
+haslo[16] = "overwhelmed";
+haslo[17] = "factor";
+
+haslo[18] = "wynik końcowy";
+haslo[19] = "naciągany";
+haslo[20] = "rozpowszechniać się";
+haslo[21] = "gruzy";
+haslo[22] = "rozkład";
+haslo[23] = "gęsty";
+haslo[24] = "ołów";
+haslo[25] = "cząsteczki";
+haslo[26] = "wysoko wiejące";
+haslo[27] = "solidny";
+haslo[28] = "słaby";
+haslo[29] = "wytyczne";
+haslo[30] = "beton";
+haslo[31] = "CV";
+haslo[32] = "prześwietlenie (czegoś, kogoś)";
+haslo[33] = "wskazówka";
+haslo[34] = "przytłoczony";
+haslo[35] = "czynnik";
+
+haslo[36] = "solution";
+haslo[37] = "assumed";
+haslo[38] = "incapable";
+haslo[39] = "recall";
+haslo[40] = "highlights";
+haslo[41] = "bracket";
+haslo[42] = "arranged";
+haslo[43] = "rely";
+haslo[44] = "surveyed";
+haslo[45] = "phone";
+haslo[46] = "expressed";
+haslo[47] = "numbers";
+haslo[48] = "devices";
+haslo[49] = "average";
+haslo[50] = "conducted";
+
+haslo[51] = "rozwiązanie";
+haslo[52] = "założyć coś";
+haslo[53] = "niezdolny";
+haslo[54] = "przypomnieć";
+haslo[55] = "podkreślać";
+haslo[56] = "przedział";
+haslo[57] = "zaaranżowany";
+haslo[58] = "polegać";
+haslo[59] = "ankietowany";
+haslo[60] = "dzwonić";
+haslo[61] = "wyrażony";
+haslo[62] = "liczby";
+haslo[63] = "urządzenia";
+haslo[64] = "przeciętny";
+haslo[65] = "przeprowadzić";
+
+
+
+
+
+
+
+
+
+var tipsy = new Array(ile_hasel);
+// Słówka z zajęć angielskiego
+tipsy[0] = "wynik końcowy, produkcja czegoś (Nasza produkcja wzrosła dwukrotnie w porównaniu z zeszłym rokiem.)";
+tipsy[1] = "naciągany, przesadny, nieprzekonujący, zbyt daleko idący (o wnioskach)";
+tipsy[2] = "rozrastać się, rozprzestrzeniać się, mnożyć się (np. o pubach), rozchodzić się (o wieściach)";
+tipsy[3] = "gruzy, rumowisko, odłamki, szczątki (np. szkła, metalu)";
+tipsy[4] = "rozkładać się, gnić (np. ciało, materiał organiczny)";
+tipsy[5] = "zwarty, zbity, gęsty (np. tłum), gęsty (o substancji, np. dym)";
+tipsy[6] = "ołów";
+tipsy[7] = "cząsteczka, cząstka";
+tipsy[8] = "wysoko wiejące (np. wiatry)";
+tipsy[9] = "zdrowy, silny, krzepki, rosły (o człowieku), solidny (o urządzeniu), zdecydowany";
+tipsy[10] = "cienki, lekki (o ubraniach), kruchy, nietrwały , marny, słaby (o argumentacji, wyjaśnieniu, wymówce)";
+tipsy[11] = "wytyczne, instrukcje";
+tipsy[12] = "beton";
+tipsy[13] = "Cirriculum Vitae (CV)";
+tipsy[14] = "prześwietlenie (analizowanie czegoś, kogoś), poddawanie kontroli";
+tipsy[15] = "oznaka, znak, wskazanie, wskazówka, zwiastun, przejaw, objaw";
+tipsy[16] = "przytłoczony";
+tipsy[17] = "czynnik, współczynnik, dzielnik (jakiejś liczby) 6 is a (...) of 24";
+
+tipsy[18] = "output, (Our output has increased two times in comparison to the last year.)";
+tipsy[19] = "far-fetched (idea, story)";
+tipsy[20] = "proliferate (It was also a time when drugs proliferated in Hollywood.)";
+tipsy[21] = "debris (It took a week to clean up the debris after the explosion.)";
+tipsy[22] = "decay (The temperatures on Mount Everest are so low that nothing there decays.)";
+tipsy[23] = "dense (Population in this area is very dense.)";
+tipsy[24] = "lead (Lead can be used to produce ammunition.) [Czytaj 'led']";
+tipsy[25] = "particles (Dr. Lloyd studies the movement of particles.)";
+tipsy[26] = "high-attitude";
+tipsy[27] = "robust (He looks robust and healthy enough.)";
+tipsy[28] = "flimsy (We spent the night in a flimsy wooden hut.)";
+tipsy[29] = "guidelines (These will be our major guidelines for action over the next five years.)";
+tipsy[30] = "concrete (She fell on a concrete pavement.)";
+tipsy[31] = "resume (You should update your resume.)";
+tipsy[32] = "screening (I hate airport screenings.)";
+tipsy[33] = "indication (There are few indications that the economy is on an upswing.)";
+tipsy[34] = "overwhelmed (If you start to feel overwhelmed, try to focus on one day at a time. )";
+tipsy[35] = "factor (Many factors contributed to this decision.)";
+
+tipsy[36] = "rozwiązanie (problemu)";
+tipsy[37] = "hipotetyczny, z góry założony (wynik czegoś), domniemany (o niewinności)";
+tipsy[38] = "niezdolny (np. do czegoś)";
+tipsy[39] = "przypomnienie sobie (czegoś)";
+tipsy[40] = "podkreślać, uwydatniać, uwypuklać (czasownik)";
+tipsy[41] = "(dosłownie) nawias, (może to być również przedział czegoś)";
+tipsy[42] = "zorganizować, planować, załatwiać";
+tipsy[43] = "polegać (na kimś, na czymś) ";
+tipsy[44] = "być badanym, być ankietowanym, ankietowani";
+tipsy[45] = "telefonować, dzwonić, telefonować do, dzwonić do";
+tipsy[46] = "wyrażony, okazany";
+tipsy[47] = "liczby";
+tipsy[48] = "urządzenia, przyrządy";
+tipsy[49] = "przeciętny, średni (np. średniego wzrostu)";
+tipsy[50] = "wykonany (przeprowadzać coś)";
+
+tipsy[51] = "solution (Cell phones, Blackberries, and other memory devices offer a solution.)";
+tipsy[52] = "assumed (His innocence is only assumed.)"
+tipsy[53] = "incapable (But most people have become incapable of remembering all this information.)";
+tipsy[54] = "recall (About one third under the age 30 couldn't recall their home phone numbers.)";
+tipsy[55] = "highlights (There are three points that I would like to highlight.)";
+tipsy[56] = "bracket (In the same age bracket, only 40% could give important dates, such as...)";
+tipsy[57] = "arranged (The study arranged the figures into age groups.)";
+tipsy[58] = "rely (We rely too much on technology.)";
+tipsy[59] = "surveyed (And only a third of those surveyed answered correctly.)";
+tipsy[60] = "phone (I'll phone you tomorrow.)";
+tipsy[61] = "expressed (It highllights what many people also expressed as 'information overload'.)";
+tipsy[62] = "numbers (These numbers make no sense.)";
+tipsy[63] = "devices (Robertson said that people ues the devices instead of their memory.)";
+tipsy[64] = "average (Devices offer a solution to the numbers and codes the average person has to remember.)";
+tipsy[65] = "conducted (Professor Ian Robertson of Trinity College Dublin conducted the study.)";
