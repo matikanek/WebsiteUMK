fa9555db34bcde41eee76c02568b97e2e3fdf2c8
matikanek <291564.umk@gmail.com>
2020-05-30 14:47:34 +0200

    Adding 7 part.

---
+++ hasla/czlowiek.js
@@ -0,0 +1,298 @@
+ile_hasel = 129;
+
+var haslo = new Array(ile_hasel);
+// Personal data / Dane osobowe
+haslo[0] = "address";
+haslo[1] = "age";
+haslo[2] = "date of birth";
+haslo[3] = "divorced";
+haslo[4] = "female";
+haslo[5] = "first name";
+haslo[6] = "gender";
+haslo[7] = "male";
+haslo[8] = "marital status";
+haslo[9] = "married";
+haslo[10] = "nationality";
+haslo[11] = "occupation";
+haslo[12] = "single";
+haslo[13] = "surname";
+haslo[14] = "widowed";
+haslo[15] = "country of residence";
+haslo[16] = "distinguishing features";
+haslo[17] = "ethnic origin";
+haslo[18] = "next of kin";
+
+haslo[19] = "adres";
+haslo[20] = "wiek";
+haslo[21] = "data urodzin";
+haslo[22] = "rozwiedziony";
+haslo[23] = "kobieta";
+haslo[24] = "imię";
+haslo[25] = "płeć";
+haslo[26] = "mężczyzna";
+haslo[27] = "stan cywilny";
+haslo[28] = "żonaty/zamężna";
+haslo[29] = "narodowość";
+haslo[30] = "zawód";
+haslo[31] = "stan wolny";
+haslo[32] = "nazwisko";
+haslo[33] = "owdowiały";
+haslo[34] = "kraj zamieszkania";
+haslo[35] = "znaki szczególne";
+haslo[36] = "pochodzenie etniczne";
+haslo[37] = "najbliższy krewny";
+
+// Appearance / Wygląd zewnętrzny
+haslo[38] = "attractive";
+haslo[39] = "unattractive";
+haslo[40] = "beautiful";
+haslo[41] = "fit";
+haslo[42] = "good-looking";
+haslo[43] = "handsome";
+haslo[44] = "plain";
+haslo[45] = "pretty";
+haslo[46] = "ugly";
+
+haslo[47] = "atrakcyjny";
+haslo[48] = "nieatrakcyjny";
+haslo[49] = "piękny";
+haslo[50] = "sprawny, w dobrej formie";
+haslo[51] = "ładny";
+haslo[52] = "przystojny";
+haslo[53] = "pospolity, nieatrakcyjny";
+haslo[54] = "ładny";
+haslo[55] = "brzydki";
+
+// Appearance expressions / Zwroty związane z wyglądem
+haslo[56] = "cut down on";
+haslo[57] = "go for";
+haslo[58] = "paint nails";
+haslo[59] = "put on weight";
+haslo[60] = "lose weight";
+haslo[61] = "straighten";
+haslo[62] = "take up (a hobby / a sport)";
+
+haslo[63] = "ograniczać";
+haslo[64] = "wybierać, sięgać po";
+haslo[65] = "malować paznokcie";
+haslo[66] = "przybierać na wadze";
+haslo[67] = "tracić na wadze";
+haslo[68] = "prostować";
+haslo[69] = "zacząć uprawiać (hobby / sport)";
+
+// Build / Budowa ciała
+haslo[70] = "fat";
+haslo[71] = "muscular";
+haslo[72] = "obese";
+haslo[73] = "overweight";
+haslo[74] = "underweight";
+haslo[75] = "plum"
+haslo[76] = "skinny";
+haslo[77] = "slim";
+haslo[78] = "well-built";
+
+haslo[79] = "gruby";
+haslo[80] = "umięśniony";
+haslo[81] = "otyły";
+haslo[82] = "nadwaga";
+haslo[83] = "niedowaga";
+haslo[84] = "puszysty / pulchny";
+haslo[85] = "chudy";
+haslo[86] = "szczupły";
+haslo[87] = "dobrze zbudowany";
+
+haslo[88] = "lanky";
+haslo[89] = "stocky";
+haslo[90] = "tyczkowaty";
+haslo[91] = "krępy";
+
+// Hair / Włosy
+haslo[92] = "bald";
+haslo[93] = "blond";
+haslo[94] = "curly";
+haslo[95] = "dark";
+haslo[96] = "dyed";
+haslo[97] = "red";
+haslo[98] = "shoulder-length";
+haslo[99] = "straight";
+haslo[100] = "wavy";
+haslo[101] = "auburn";
+haslo[102] = "fine";
+haslo[103] = "thick";
+haslo[104] = "fringe";
+haslo[105] = "have a centre parting";
+haslo[106] = "have a side parting";
+haslo[107] = "ponytail";
+haslo[108] = "receding hairline";
+haslo[109] = "wear hair loose";
+haslo[110] = "wear hair up";
+
+haslo[111] = "łysy";
+haslo[112] = "blond";
+haslo[113] = "kręcone";
+haslo[114] = "ciemne";
+haslo[115] = "farbowane";
+haslo[116] = "rude";
+haslo[117] = "do ramion";
+haslo[118] = "proste";
+haslo[119] = "falujące";
+haslo[120] = "kasztanowe";
+haslo[121] = "cienkie, rzadkie";
+haslo[122] = "gęste";
+haslo[123] = "grzywka";
+haslo[124] = "mieć przedziałek na środku";
+haslo[125] = "mieć przedziałek z boku";
+haslo[126] = "kitka, koński ogon";
+haslo[127] = "włosy rzednące na skroniach";
+haslo[128] = "nosić włosy rozpuszczone";
+haslo[129] = "nosić włosy spięte";
+
+
+
+
+var tipsy = new Array(ile_hasel);
+// Personal data / Dane osobowe
+tipsy[0] = "adres (Dane osobowe)";
+tipsy[1] = "wiek (Dane osobowe)";
+tipsy[2] = "data urodzin (Dane osobowe)";
+tipsy[3] = "rozwiedziony (Dane osobowe)";
+tipsy[4] = "kobieta (Dane osobowe)";
+tipsy[5] = "imię (Dane osobowe)";
+tipsy[6] = "płeć (Dane osobowe)";
+tipsy[7] = "mężczyzna (Dane osobowe)";
+tipsy[8] = "stan cywilny (Dane osobowe)";
+tipsy[9] = "żonaty/zamężna (Dane osobowe)";
+tipsy[10] = "narodowość (Dane osobowe)";
+tipsy[11] = "zawód (Dane osobowe)";
+tipsy[12] = "stan wolny (Dane osobowe)";
+tipsy[13] = "nazwisko (Dane osobowe)";
+tipsy[14] = "owdowiały (Dane osobowe)";
+tipsy[15] = "kraj zamieszkania (Dane osobowe)";
+tipsy[16] = "znaki szczególne (Dane osobowe)";
+tipsy[17] = "pochodzenie etniczne (Dane osobowe)";
+tipsy[18] = "najbliższy krewny (Dane osobowe)";
+
+tipsy[19] = "address (Personal data)";
+tipsy[20] = "age (Personal data)";
+tipsy[21] = "date of birth (Personal data)";
+tipsy[22] = "divorced (Personal data)";
+tipsy[23] = "female (Personal data)";
+tipsy[24] = "first name (Personal data)";
+tipsy[25] = "gender (Personal data)";
+tipsy[26] = "male (Personal data)";
+tipsy[27] = "marital status (Personal data)";
+tipsy[28] = "married (Personal data)";
+tipsy[29] = "nationality (Personal data)";
+tipsy[30] = "occupation (Personal data)";
+tipsy[31] = "single (Personal data)";
+tipsy[32] = "surname (Personal data)";
+tipsy[33] = "widowed (Personal data)";
+tipsy[34] = "country of residence (Personal data)";
+tipsy[35] = "distinguishing features (Personal data)";
+tipsy[36] = "ethnic origin (Personal data)";
+tipsy[37] = "next of kin (Personal data)";
+
+// Appearance / Wygląd zewnętrzny
+tipsy[38] = "atrakcyjny (Wygląd zewnętrzny)";
+tipsy[39] = "nieatrakcyjny (Wygląd zewnętrzny)";
+tipsy[40] = "piękny (Wygląd zewnętrzny)";
+tipsy[41] = "sprawny, w dobrej formie (Wygląd zewnętrzny)";
+tipsy[42] = "przystojny, ładny (Wygląd zewnętrzny)";
+tipsy[43] = "przystojny (Wygląd zewnętrzny)";
+tipsy[44] = "pospolity, nieatrakcyjny (Wygląd zewnętrzny)";
+tipsy[45] = "ładny (Wygląd zewnętrzny)";
+tipsy[46] = "brzydki (Wygląd zewnętrzny)";
+
+tipsy[47] = "attractive (Appearance)";
+tipsy[48] = "unattractive (Appearance)";
+tipsy[49] = "beautiful (Appearance)";
+tipsy[50] = "fit (Appearance)";
+tipsy[51] = "good-looking (Appearance)";
+tipsy[52] = "handsome (Appearance)";
+tipsy[53] = "plain (Appearance)";
+tipsy[54] = "pretty (Appearance)";
+tipsy[55] = "ugly (Appearance)";
+
+// Appearance expressions / Zwroty związane z wyglądem
+tipsy[56] = "ograniczać (ilość czegoś) (Zwroty związane z wyglądem)";
+tipsy[57] = "wybierać, sięgać po (Zwroty związane z wyglądem)";
+tipsy[58] = "malować paznokcie (Zwroty związane z wyglądem)";
+tipsy[59] = "przybierać na wadze (Zwroty związane z wyglądem)";
+tipsy[60] = "tracić na wadze (Zwroty związane z wyglądem)";
+tipsy[61] = "prostować (włosy) (Zwroty związane z wyglądem)";
+tipsy[62] = "zacząć uprawiać (hobby / sport) (Zwroty związane z wyglądem)";
+
+tipsy[63] = "cut down on (Appearance expressions)";
+tipsy[64] = "go for (Appearance expressions)";
+tipsy[65] = "paint nails (Appearance expressions)";
+tipsy[66] = "put on weight (Appearance expressions)";
+tipsy[67] = "lose weight (Appearance expressions)";
+tipsy[68] = "straighten (Appearance expressions)";
+tipsy[69] = "take up (a hobby / a sport) (Appearance expressions)";
+
+// Build / Budowa ciała
+tipsy[70] = "gruby (Budowa ciała)";
+tipsy[71] = "umięśniony (Budowa ciała)";
+tipsy[72] = "otyły (Budowa ciała)";
+tipsy[73] = "nadwaga (Budowa ciała)";
+tipsy[74] = "niedowaga (Budowa ciała)";
+tipsy[75] = "puszysty / pulchny (Budowa ciała)";
+tipsy[76] = "chudy (Budowa ciała)";
+tipsy[77] = "szczupły (Budowa ciała)";
+tipsy[78] = "dobrze zbudowany (Budowa ciała)";
+
+tipsy[79] = "fat (Build)";
+tipsy[80] = "muscular (Build)";
+tipsy[81] = "obese (Build)";
+tipsy[82] = "overweight (Build)";
+tipsy[83] = "underweight (Build)";
+tipsy[84] = "plum (Build)"
+tipsy[85] = "skinny (Build)";
+tipsy[86] = "slim (Build)";
+tipsy[87] = "well-built (Build)";
+
+tipsy[88] = "tyczkowaty, wysoki i chudy (Budowa ciała)";
+tipsy[89] = "krępy (Budowa ciała)";
+tipsy[90] = "lanky (Build)";
+tipsy[91] = "stocky (Build)";
+
+// Hair / Włosy
+tipsy[92] = "łysy (Włosy)";
+tipsy[93] = "blond (Włosy)";
+tipsy[94] = "kręcone (Włosy)";
+tipsy[95] = "ciemne (Włosy)";
+tipsy[96] = "farbowane (Włosy)";
+tipsy[97] = "rude (Włosy)";
+tipsy[98] = "do ramion (Włosy)";
+tipsy[99] = "proste (Włosy)";
+tipsy[100] = "falujące (Włosy)";
+tipsy[101] = "kasztanowe (Włosy)";
+tipsy[102] = "cienkie, rzadkie (Włosy)";
+tipsy[103] = "gęste (Włosy)";
+tipsy[104] = "grzywka (Włosy)";
+tipsy[105] = "mieć przedziałek na środku (Włosy)";
+tipsy[106] = "mieć przedziałek z boku (Włosy)";
+tipsy[107] = "kitka, koński ogon (Włosy)";
+tipsy[108] = "włosy rzednące na skroniach (Włosy)";
+tipsy[109] = "nosić włosy rozpuszczone (Włosy)";
+tipsy[110] = "nosić włosy spięte (Włosy)";
+
+tipsy[111] = "bald (Hair)";
+tipsy[112] = "blond (Hair)";
+tipsy[113] = "curly (Hair)";
+tipsy[114] = "dark (Hair)";
+tipsy[115] = "dyed (Hair)";
+tipsy[116] = "red (Hair)";
+tipsy[117] = "shoulder-length (Hair)";
+tipsy[118] = "straight (Hair)";
+tipsy[119] = "wavy (Hair)";
+tipsy[120] = "auburn (Hair)";
+tipsy[121] = "fine (Hair)";
+tipsy[122] = "thick (Hair)";
+tipsy[123] = "fringe (Hair)";
+tipsy[124] = "have a centre parting (Hair)";
+tipsy[125] = "have a side parting (Hair)";
+tipsy[126] = "ponytail (Hair)";
+tipsy[127] = "receding hairline (Hair)";
+tipsy[128] = "wear hair loose (Hair)";
+tipsy[129] = "wear hair up (Hair)";
---
+++ hasla/szkola.js
@@ -0,0 +1,103 @@
+ile_hasel = 28;
+
+var haslo = new Array(ile_hasel);
+// School subjects / Przedmioty szkolne
+haslo[0] = "art subject";
+haslo[1] = "biology";
+haslo[2] = "chemistry";
+haslo[3] = "double lesson";
+haslo[4] = "foreign languages";
+haslo[5] = "geography";
+haslo[6] = "history";
+haslo[7] = "humanities";
+haslo[8] = "IT / information technology";
+haslo[9] = "mathematics (maths)";
+haslo[10] = "music";
+haslo[11] = "PE / physical education";
+haslo[12] = "physics";
+haslo[13] = "Polish";
+haslo[14] = "religious education";
+haslo[15] = "science subjects";
+haslo[16] = "social sciences";
+haslo[17] = "design and technology";
+haslo[18] = "economic education";
+haslo[19] = "enviromental science";
+haslo[20] = "media studies";
+haslo[21] = "sports psychology";
+
+haslo[22] = "przedmioty artystyczne";
+haslo[23] = "biologia";
+haslo[24] = "chemia";
+haslo[25] = "podwójna lekcja";
+haslo[26] = "języki obce";
+haslo[27] = "geografia";
+haslo[28] = "historia";
+haslo[29] = "nauki humanistyczne";
+haslo[39] = "informatyka";
+haslo[31] = "matematyka (matma)";
+haslo[32] = "muzyka";
+haslo[33] = "WF / wychowanie fizyczne";
+haslo[34] = "fizyka";
+haslo[35] = "język polski";
+haslo[36] = "religia";
+haslo[37] = "przedmioty ścisłe";
+haslo[38] = "nauki społeczne";
+haslo[39] = "technika";
+haslo[40] = "ekonomia";
+haslo[41] = "nauka o ochronie środowiska";
+haslo[42] = "medioznawstwo";
+haslo[43] = "psychologia sportu";
+
+
+
+
+
+
+
+var tipsy = new Array(ile_hasel);
+// School subjects / Przedmioty szkolne
+tipsy[0] = "przedmioty artystyczne";
+tipsy[1] = "biologia";
+tipsy[2] = "chemia";
+tipsy[3] = "podwójna lekcja";
+tipsy[4] = "języki obce";
+tipsy[5] = "geografia";
+tipsy[6] = "historia";
+tipsy[7] = "nauki humanistyczne";
+tipsy[8] = "informatyka";
+tipsy[9] = "matematyka (matma)";
+tipsy[10] = "muzyka";
+tipsy[11] = "WF / wychowanie fizyczne";
+tipsy[12] = "fizyka";
+tipsy[13] = "język polski";
+tipsy[14] = "religia";
+tipsy[15] = "przedmioty ścisłe";
+tipsy[16] = "nauki społeczne";
+tipsy[17] = "technika";
+tipsy[18] = "ekonomia";
+tipsy[19] = "nauka o ochronie środowiska";
+tipsy[20] = "medioznawstwo";
+tipsy[21] = "psychologia sportu";
+
+tipsy[22] = "art subject";
+tipsy[23] = "biology";
+tipsy[24] = "chemistry";
+tipsy[25] = "double lesson";
+tipsy[26] = "foreign languages";
+tipsy[27] = "geography";
+tipsy[28] = "history";
+tipsy[29] = "humanities";
+tipsy[30] = "IT / information technology";
+tipsy[31] = "mathematics (maths)";
+tipsy[32] = "music";
+tipsy[33] = "PE / physical education";
+tipsy[34] = "physics";
+tipsy[35] = "Polish";
+tipsy[36] = "religious education";
+tipsy[37] = "science subjects";
+tipsy[38] = "social sciences";
+tipsy[39] = "design and technology";
+tipsy[40] = "economic education";
+tipsy[41] = "enviromental science";
+tipsy[42] = "media studies";
+tipsy[43] = "sports psychology";
